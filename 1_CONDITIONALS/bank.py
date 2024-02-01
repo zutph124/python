b2097b653f256893d48d2815cca1f4a826f61091
zutph124 <zutph124@gmail.com>
2024-02-02 00:27:25 +0100

    Update bank.py

--- 1_CONDITIONALS/bank.py
+++ 1_CONDITIONALS/bank.py
@@ -1,1 +1,15 @@
+'''
+In a file called bank.py, implement a program that prompts the user for a greeting. 
+If the greeting starts with “hello”, output $0. 
+If the greeting starts with an “h” (but not “hello”), output $20. 
+Otherwise, output $100. 
 
+Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
+'''
+
+greeting = input("Greeting: ").strip().lower()
+
+if greeting.startswith("h",0):
+    if greeting.startswith("hello",0,5): print("$0")
+    elif greeting != "hello": print("$20")
+else: print("$100")
