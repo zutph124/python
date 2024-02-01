f85ba13fb155f0ef8a3bb1a5b19806b3b98d9e82
zutph124 <zutph124@gmail.com>
2024-02-02 00:07:05 +0100

    Update faces.py

--- 0_FUNCTIONS VARIABLES/faces.py
+++ 0_FUNCTIONS VARIABLES/faces.py
@@ -1,1 +1,22 @@
+'''
+In a file called faces.py, implement a function called convert that accepts a str as input and returns 
+that same input with any :) converted to 🙂 (otherwise known as a slightly smiling face) and 
+any :( converted to 🙁 (otherwise known as a slightly frowning face). All other text should be returned unchanged.
+ 
+Then, in that same file, implement a function called main that prompts the user for input, 
+calls convert on that input, and prints the result. You’re welcome, but not required, to prompt the user explicitly, 
+as by passing a str of your own as an argument to input. Be sure to call main at the bottom of you
+'''
+
+def main():
+    text = "happy :) and sad :("
+#   text = input("Enter text: ")
+    convert(text)
+
+def convert(text):
+    text = text.replace(":)", "🙂")
+    text = text.replace(":(", "🙁")
+    print(text)
+
+main()
 
