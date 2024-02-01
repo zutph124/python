239fc24e6258e4a2a62f9c012578f2e20ab281d4
zutph124 <zutph124@gmail.com>
2024-02-02 00:38:48 +0100

    Update deep.py

--- 1_CONDITIONALS/deep.py
+++ 1_CONDITIONALS/deep.py
@@ -4,3 +4,11 @@ Great Question of Life, the Universe and Everything, outputting Yes if the user
 inputs 42 or (case-insensitively) forty-two or forty two. 
 Otherwise output No.
 '''
+
+
+#list
+values = ["42", "forty-two", "forty two"]
+if answer in values:
+   print("Yes")
+else: print("No")
+  
