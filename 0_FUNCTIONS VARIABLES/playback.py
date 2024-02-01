ea775a7f5ca26e6351b1c3952f1058f1a89f3d65
zutph124 <zutph124@gmail.com>
2024-02-02 00:18:32 +0100

    Create playback.py

---
+++ 0_FUNCTIONS VARIABLES/playback.py
@@ -0,0 +1,8 @@
+'''
+In a file called playback.py, implement a program in Python that prompts the user for input 
+and then outputs that same input, replacing each space with ... (i.e., three periods).
+'''
+
+
+text = input("Enter text: ")
+print(text.replace(" ","..."))
