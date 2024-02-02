6e25fe5a369ad5864c06606deb9858339c5c70fa
zutph124 <zutph124@gmail.com>
2024-02-02 01:15:12 +0100

    Create twttr.py

---
+++ 2_LOOPS/twttr.py
@@ -0,0 +1,9 @@
+string = input("Input : ")
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+result = ""
+
+for i in range(len(string)):
+    if string[i] not in vowels:
+        result = result + string[i]
+
+print("Output: ", result)
