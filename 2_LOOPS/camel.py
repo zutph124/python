de023ee3dcfe66a9b42c5303c38c9a9ade391b22
zutph124 <zutph124@gmail.com>
2024-02-02 01:10:39 +0100

    Update camel.py

--- 2_LOOPS/camel.py
+++ 2_LOOPS/camel.py
@@ -1,2 +1,16 @@
 '''
+In a file called camel.py, implement a program that prompts the user for the name of a variable in camel case
+and outputs the corresponding name in snake case.
+
+preferredFirstName --> preferred_first_name
 '''
+charC = ""
+charS = ""
+s = input("camelCase: ")
+for char in s:
+    charC += char
+    if char.isupper():
+        charS += "_" + char.lower()
+    else: charS += char
+print("camelCase: ", charC)
+print("snake_case:", charS)
