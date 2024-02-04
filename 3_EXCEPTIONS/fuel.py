d863eed5f1b63f6c542bf0042721e2a30214f64b
zutph124 <zutph124@gmail.com>
2024-02-04 18:21:14 +0100

    Update fuel.py

--- 3_EXCEPTIONS/fuel.py
+++ 3_EXCEPTIONS/fuel.py
@@ -8,27 +8,26 @@
 
 
 while True:
-   x, y  = input("Fraction: ").split('/')
-   x = int(x)
-   y = int(y)
 
    try:
+       x, y  = input("Fraction: ").split('/')
+       x = int(x)
+       y = int(y)
        dec_n = round(x/y,2)
        if x > y: continue
+       else: break
    except ValueError: pass
    except ZeroDivisionError: pass
 
-   else:
-       break
-
 #
-# no need to print out percentages if fuel gauge is full or empty
+# no need to print out percentages if fuel gauge if full or empty
 #
 if dec_n <=  1/100: print("E")
 if dec_n >= 99/100: print("F")
 percentage = f"{dec_n:.0%}"
 
 #
-# print out percentages if fuel gauge is neither full or empty
+# print out percentages if fuel gauge is niether full or empty
 #
 if (dec_n > 1/100 and dec_n < 99/100): print(percentage)
+
