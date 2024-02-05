bdbdc53753bda0093abc30373f55985c314a8ca9
zutph124 <zutph124@gmail.com>
2024-02-05 10:18:43 +0100

    Update grocery.py

--- 3_EXCEPTIONS/grocery.py
+++ 3_EXCEPTIONS/grocery.py
@@ -1,1 +1,25 @@
+"""
+In a file called grocery.py, implement a program that prompts the user for items,
+one per line, until the user inputs control-d (which is a common way of ending one’s input to a program).
+Then output the user’s grocery list in all uppercase, sorted alphabetically by item,
+prefixing each line with the number of times the user inputted that item.
+No need to pluralize the items. Treat the user’s input case-insensitively.
+"""
+
+
+#dictionary
+mylist = {}
+
+while True:
+    try:
+        str = input().upper()              # convert to UPPERCASE
+        if str in mylist:
+            mylist[str] = mylist[str] + 1
+        else:
+            mylist[str] = 1
+    except EOFError:
+        mylist = dict(sorted(mylist.items())) # dictionary sorted
+        for i in mylist:
+            print(mylist[i], i)
+        break
 
