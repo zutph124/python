32addca1eb25b174bfcb45ec0ae694d957343f0f
zutph124 <zutph124@gmail.com>
2024-02-11 20:52:57 +0100

    Create adieu.py

---
+++ 4_LIBRARIES/adieu.py
@@ -0,0 +1,27 @@
+'''
+In a file called adieu.py, implement a program that prompts the user for names, one per line, until the user inputs control-d. 
+Assume that the user will input at least one name. Then bid adieu to those names, separating two names with one and, 
+three names with two commas and one and, and names with commas and one and, as in the below:
+  Adieu, adieu, to Liesl
+  Adieu, adieu, to Liesl and Friedrich
+  Adieu, adieu, to Liesl, Friedrich, and Louisa
+  Adieu, adieu, to Liesl, Friedrich, Louisa, and Kurt
+  Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, and Brigitta
+  Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, and Marta
+  Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, Marta, and Gretl
+'''
+
+
+import inflect
+
+p = inflect.engine()
+
+names = []
+
+while True:
+    try:
+        str = input("Name: ")
+        names.append(str)
+    except EOFError:
+        print("Adieu, adieu, to",p.join(names))
+        break
